d8013afa67e5f20a0e49d235330fe129ecdda3fc
Shreyas Tripathy <shreyastripathy@outlook.com>
2026-04-14 02:27:05 +0530

    removed show more - script.js

--- script.js
+++ script.js
@@ -138,26 +138,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-  /* =====================
-     EXPERIENCE
-  ===================== */
-
-  const toggleBtn = document.querySelector(".timeline-toggle");
-  const moreSection = document.querySelector(".timeline-more");
-
-  if (toggleBtn && moreSection) {
-    toggleBtn.addEventListener("click", () => {
-      const isHidden = moreSection.classList.contains("hidden");
-
-      if (isHidden) {
-        moreSection.classList.remove("hidden");
-        toggleBtn.textContent = "Show less ↑";
-      } else {
-        moreSection.classList.add("hidden");
-        toggleBtn.textContent = "Show more ↓";
-      }
-    });
-  }
 
   /* =====================
      NAV UNDERLINE
